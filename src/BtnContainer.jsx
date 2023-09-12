9732038d74ad284b6e6e4c79a496f953a231f156
Karin Marjieh <karinmarjieh@hotmail.com>
2023-09-12 18:56:04 +0300

    added files

---
+++ src/BtnContainer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const BtnContainer = ({ jobs, currentItem, setCurrentItem }) => {
+  return (
+    <div className="btn-container">
+      {jobs.map((job, index) => {
+        return (
+          <button
+            key={job.id}
+            onClick={() => {
+              setCurrentItem(index);
+            }}
+            className={index === currentItem ? 
+            "job-btn active-btn" : "job-btn"}
+          >
+            {job.company}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
